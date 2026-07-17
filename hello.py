90f4f28b3d633e935e34b90528eb83e1576858b3
nihalapkd2008-debug <nihalapkd2008@gmail.com>
2026-07-17 10:24:54 +0530

    first commit

--- hello.py
+++ hello.py
@@ -1,8 +1,8 @@
+name = "Nihala"
 age = 18
-price = 18.0
-name = "18"
+height = 18.6
 is_student = True
 print(type(age))
-print(type(price))
+print(type(height))
 print(type(name))
 print(type(is_student))
